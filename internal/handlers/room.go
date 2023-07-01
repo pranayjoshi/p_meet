91457a954237507a6bc13560aa428133b78965dc
pranayjoshi <pranayjoshi4466@gmail.com>
2023-07-01 21:35:53 +0530

    added

---
+++ internal/handlers/room.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+	guuid "github.com/google/uuid"
+)
+
+func RoomCreate(c *fiber.Ctx) error {
+	return c.Redirect(fmt.Sprintf("/room/%s", guuid.New().String()))
+}
+
+func Room(c *fiber.Ctx) error {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		c.Status(404)
+		return nil
+	}
+	// _, _, room := createOrGetRoom(uuid);
+}
+
+func RoomWebsocket(c *websocket.Conn) {
+	uuid := c.Params("uuid")
+	if uuid == "" {
+		return
+	}
+
+	// _, _, room := createOrGetRoom(uuid)
+	// w.RoomConn(c, room.Peers)
+}
