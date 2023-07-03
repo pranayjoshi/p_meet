3f055d5c42c964292c48cd2c27ebcd8a41afe958
pranayjoshi <pranayjoshi4466@gmail.com>
2023-07-03 06:31:06 +0530

    added

--- pkg/chat/client.go
+++ pkg/chat/client.go
@@ -3,11 +3,23 @@ package chat
 import (
 	"p_meet/models"
 	"time"
-	// "github.com/fasthttp/websocket"
+
+	// "github.com/gofiber/websocket/v2"
+	"github.com/fasthttp/websocket"
 )
 
 type Client struct {
 	*models.Client
+}
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+var upgrader = websocket.FastHTTPUpgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
 const (
