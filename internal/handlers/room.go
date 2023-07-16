d8e54ccb20581d59c0f3ea489020c78d62661ef8
pranayjoshi <pranayjoshi4466@gmail.com>
2023-07-16 19:00:01 +0530

    added

--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"p_meet/models"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
@@ -72,4 +73,21 @@ func RoomViewerWebsocket(c *websocket.Conn, p *models.Peers) {
 		return
 	}
 	w.RoomsLock.Unlock()
+}
+
+func roomViewerConn(c *websocket.Conn, p *models.Peers) {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+	defer c.Close()
+
+	for {
+		select {
+		case <-ticker.C:
+			w, err := c.Conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write([]byte(fmt.Sprintf("%d", len(p.Connections))))
+		}
+	}
 }
