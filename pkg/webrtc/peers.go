511af3ccedbf4dbfe8ecbcd0b7de69931063ab22
pranayjoshi <pranayjoshi4466@gmail.com>
2023-07-17 18:00:05 +0530

    added

--- pkg/webrtc/peers.go
+++ pkg/webrtc/peers.go
@@ -2,11 +2,14 @@ package webrtc
 
 import (
 	// "p_meet/models"
+	"encoding/json"
 	"log"
 	"p_meet/pkg/chat"
 	"sync"
+	"time"
 
 	"github.com/gofiber/websocket/v2"
+	"github.com/pion/rtcp"
 	"github.com/pion/webrtc/v3"
 )
 
@@ -31,8 +34,10 @@ type ThreadSafeWriter struct {
 	Mutex sync.Mutex
 }
 
-func (p *Peers) DispatchKeyFrame() {
-
+func (t *ThreadSafeWriter) WriteJSON(v interface{}) error {
+	t.Mutex.Lock()
+	defer t.Mutex.Unlock()
+	return t.Conn.WriteJSON(v)
 }
 
 func (p *Peers) AddTrack(t *webrtc.TrackRemote) *webrtc.TrackLocalStaticRTP {
@@ -60,4 +65,114 @@ func (p *Peers) RemoveTrack(t *webrtc.TrackLocalStaticRTP) {
 	}()
 
 	delete(p.TrackLocals, t.ID())
+}
+
+func (p *Peers) SignalPeerConnections() {
+	p.ListLock.Lock()
+	defer func() {
+		p.ListLock.Unlock()
+		p.DispatchKeyFrame()
+	}()
+
+	attemptSync := func() (tryAgain bool) {
+		for i := range p.Connections {
+			if p.Connections[i].PeerConnection.ConnectionState() == webrtc.PeerConnectionStateClosed {
+				p.Connections = append(p.Connections[:i], p.Connections[i+1:]...)
+				log.Println("a", p.Connections)
+				return true
+			}
+
+			existingSenders := map[string]bool{}
+			for _, sender := range p.Connections[i].PeerConnection.GetSenders() {
+				if sender.Track() == nil {
+					continue
+				}
+
+				existingSenders[sender.Track().ID()] = true
+
+				if _, ok := p.TrackLocals[sender.Track().ID()]; !ok {
+					if err := p.Connections[i].PeerConnection.RemoveTrack(sender); err != nil {
+						return true
+					}
+				}
+			}
+
+			for _, receiver := range p.Connections[i].PeerConnection.GetReceivers() {
+				if receiver.Track() == nil {
+					continue
+				}
+
+				existingSenders[receiver.Track().ID()] = true
+			}
+
+			for trackID := range p.TrackLocals {
+				if _, ok := existingSenders[trackID]; !ok {
+					if _, err := p.Connections[i].PeerConnection.AddTrack(p.TrackLocals[trackID]); err != nil {
+						return true
+					}
+				}
+			}
+
+			offer, err := p.Connections[i].PeerConnection.CreateOffer(nil)
+			if err != nil {
+				return true
+			}
+
+			if err = p.Connections[i].PeerConnection.SetLocalDescription(offer); err != nil {
+				return true
+			}
+
+			offerString, err := json.Marshal(offer)
+			if err != nil {
+				return true
+			}
+
+			if err = p.Connections[i].Websocket.WriteJSON(&websocketMessage{
+				Event: "offer",
+				Data:  string(offerString),
+			}); err != nil {
+				return true
+			}
+		}
+
+		return
+	}
+
+	for syncAttempt := 0; ; syncAttempt++ {
+		if syncAttempt == 25 {
+			go func() {
+				time.Sleep(time.Second * 3)
+				p.SignalPeerConnections()
+			}()
+			return
+		}
+
+		if !attemptSync() {
+			break
+		}
+	}
+}
+
+func (p *Peers) DispatchKeyFrame() {
+	p.ListLock.Lock()
+	defer p.ListLock.Unlock()
+
+	for i := range p.Connections {
+		for _, receiver := range p.Connections[i].PeerConnection.GetReceivers() {
+			if receiver.Track() == nil {
+				continue
+			}
+
+			_ = p.Connections[i].PeerConnection.WriteRTCP([]rtcp.Packet{
+				&rtcp.PictureLossIndication{
+					MediaSSRC: uint32(receiver.Track().SSRC()),
+				},
+			})
+		}
+	}
+}
+
+type websocketMessage struct {
+	Event string `json:"event"`
+	Data  string `json:"data"`
 }
